df37ec68bcefcf3abcd452febbee162a1bfe98a1
John <johnpolhill42@gmail.com>
2015-10-23 05:40:19 +0000

    add

--- week-7/gps6/my_solution.rb
+++ week-7/gps6/my_solution.rb
@@ -65,3 +65,17 @@ end
 
 #=======================================================================
 # Reflection Section
+# What are the differences between the two different hash syntaxes shown in the state_data file?
+# => One uses the symbol format and the other uses the hash rocket. 
+
+# What does require_relative do? How is it different from require?
+# => require_relative uses the path relative to the file location as opposed to the absolute path.
+
+# What are some ways to iterate through a hash?
+# => enumerable methods, while loops, etc.
+
+#When refactoring virus_effects, what stood out to you about the variables, if anything?
+# => Instance variables don't need to be passed as parameters
+
+#What concept did you most solidify in this challenge?
+# => @@ means class variables.
