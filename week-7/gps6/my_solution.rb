cee541a22f66abe8c70c39339b1d70b8dfe330c7
John <johnpolhill42@gmail.com>
2015-10-23 05:28:27 +0000

    gps session

--- week-7/gps6/my_solution.rb
+++ week-7/gps6/my_solution.rb
@@ -4,44 +4,33 @@
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
+# require_relative uses the path relative to the file location as opposed to the absolute path.
 #
 require_relative 'state_data'
 
 class VirusPredictor
-
+  #Instatiates the instance variables.
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
-
+  # Prints the results of the virus effects.
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths()
+    speed_of_spread()
   end
 
   private
-
-  def predicted_deaths(population_density, population, state)
+  # predicts the deaths based on the population and population density, and prints the result.
+  def predicted_deaths()
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
-    else
-      number_of_deaths = (@population * 0.05).floor
-    end
-
+    # Not the same as before but more accurate
+    number_of_deaths = (@population * (@population_density / 500)).floor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
-
   end
-
-  def speed_of_spread(population_density, state) #in months
+  #Predicts the speed of spreed based on population density and prints the results.
+  def speed_of_spread() #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -70,18 +59,9 @@ end
  # initialize VirusPredictor for each state
 
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
-
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
-
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
-
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
-
+STATE_DATA.each do |state_name, pop_data|
+  VirusPredictor.new(state_name, pop_data[:population_density], pop_data[:population]).virus_effects;
+end
 
 #=======================================================================
 # Reflection Section
